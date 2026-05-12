aca6bdef837b6167a5b960c1f57bac190f6882a1
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-05-12 21:15:50 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+use core::panic::PanicInfo;
+
+pub extern "system" fn driver_entry() -> u32 {
+    0
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[cfg(not(test))]
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {  }
 }
